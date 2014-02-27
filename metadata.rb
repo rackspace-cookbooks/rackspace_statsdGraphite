05dce3309a7ffac903bd2d7bfd4136c9de56dbaf
Ryan Richard <ryanrichard07@gmail.com>
2014-02-27 08:49:54 -0800

    cloud_monitoring to rackspace_cloudmonitoring
    and change from depends to suggests

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,9 @@ maintainer_email 'ryan.richard@rackspace.com'
 license          'All rights reserved'
 description      'Installs/Configures statsd_graphite-rolebook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.6'
+version          '0.0.7'
 
 depends 'graphite', '~> 0.4.3'
 depends 'statsd', '~> 0.1.0'
-depends 'cloud_monitoring', '~> 1.0.0'
+
+suggests 'rackspace_cloudmonitoring', '~> 1.0.0'
